0c79ea9c91fad49e6a1bf7f59b257065c2f7a949
borgeta-code <matheusborgescode@gmail.com>
2024-07-11 19:33:42 -0300

    aula 3

--- src/routes/get-participants.ts
+++ src/routes/get-participants.ts
@@ -1,12 +1,12 @@
 import type { FastifyInstance } from "fastify";
 import type { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
-import { ClientError } from "../errors/client-error";
 import { prisma } from "../lib/prisma";
+import { ClientError } from "../errors/client-error";
 
-export async function getLinks(app: FastifyInstance) {
+export async function getParticipants(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get(
-    "/trips/:tripId/links",
+    "/trips/:tripId/participants",
     {
       schema: {
         params: z.object({
@@ -20,7 +20,14 @@ export async function getLinks(app: FastifyInstance) {
       const trip = await prisma.trip.findUnique({
         where: { id: tripId },
         include: {
-          links: true,
+          participants: {
+            select: {
+              id: true,
+              name: true,
+              email: true,
+              is_confirmed: true,
+            },
+          },
         },
       });
 
@@ -28,7 +35,7 @@ export async function getLinks(app: FastifyInstance) {
         throw new ClientError("Trip not found");
       }
 
-      return { links: trip.links };
+      return { participants: trip.participants };
     }
   );
 }
